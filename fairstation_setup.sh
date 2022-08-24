6cab494f850567dfee60fd0e7a89478ceaa7e571
Shashank Chakravarthy <shashank.chakravarthy@gmail.com>
2022-08-24 09:25:27 +0200

    Other install dependencies and corrections

--- fairstation_setup.sh
+++ fairstation_setup.sh
@@ -3,7 +3,13 @@
 # Before running the script, make it executable chmod +x command
 # method to download and setup environment
 env_setup(){
-	$1 apt update && $1 apt install git docker
+	$1 apt update && $1 apt install apt-transport-https git ca-certificates curl gnupg lsb-release -y 
+	$1 mkdir -p /etc/apt/keyrings 
+	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | $1 gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+	echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | $1 tee /etc/apt/sources.list.d/docker.list > /dev/null
+	$1 apt update
+	$1 apt install docker-ce docker-ce-cli containerd.io docker-compose-p -y
+	# $1 usermod -aG docker $USER -- if permission becomes an issue
 	# If this docker install fails, can install docker compose through pip. Using the latest version, hopefully does not break anything else.
 	$1 curl -L https://github.com/docker/compose/releases/download/v2.10.0/docker-compose-linux-x86_64  -o /usr/local/bin/docker-compose
 	$1 chmod +x /usr/local/bin/docker-compose
