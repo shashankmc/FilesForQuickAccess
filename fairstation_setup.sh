7dd37ddcda921a4596db194f452bf15ee53b1de8
Aiara Lobo Gomes <aiara.lobogomes@maastro.nl>
2022-08-25 13:02:51 +0200

    Enforced sudo for setting up environment (redhat); added extra env vars that are necessary for fairifier2

--- fairstation_setup.sh
+++ fairstation_setup.sh
@@ -17,44 +17,62 @@ ubuntu_env_setup(){
 	$1 service docker start
 	docker-compose --version
 }
+
 redhat_env_setup(){
-	$1 yum update && $1 yum install -y yum-utils git curl wget #device-mapper-persistent-data lvm2
-	$1 yum-config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
-	$1 yum update && $1 yum install -y docker-ce docker-ce-cli containerd.io 
-	# $1 yum install docker-ce --allowerasing # if dependency issues with podman
-	$1 systemctl enable --now docker
+	sudo yum update && sudo yum install -y yum-utils git curl wget #device-mapper-persistent-data lvm2
+	sudo yum-config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+	sudo yum update && sudo yum install -y docker-ce docker-ce-cli containerd.io 
+	# sudo yum install docker-ce --allowerasing # if dependency issues with podman
+	sudo systemctl enable --now docker
 	systemctl status docker
-	$1 usermod -aG docker $USER
+	sudo usermod -aG docker $USER
 	newgrp docker
 	id $USER
 	docker version
 	curl -s https://api.github.com/repos/docker/compose/releases/latest | grep browser_download_url  | grep docker-compose-linux-x86_64 | cut -d '"' -f 4 | wget -qi -
 	chmod +x docker-compose-linux-x86_64
-	$1 mv docker-compose-linux-x86_64 /usr/local/bin/docker-compose
+	sudo mv docker-compose-linux-x86_64 /usr/local/bin/docker-compose
 	docker-compose version
 }
+
 docker_setup(){
-	$1 docker-compose up -d --quiet-pull
+	docker-compose up -d --quiet-pull
 }
+
 repo_setup(){
 	git clone https://gitlab.com/UM-CDS/protrait/fairifier2.git
 	cd fairifier2
 	# if additional env variables need to be added, add here 	
 	touch .env
-	echo "\nAIRFLOW_UID=50000" >> .env
-	echo "\nAIRFLOW_GID=0" >> .env
-	echo "\nR2RML_RDB_CONNSTR=jdbc:postgresql://localhost:5432/protrait" >> .env
-	echo "\nR2RML_RDB_PASSWORD=passwordhere" >> .env
-	echo "\nR2RML_RDB_USER=postgres" >> .env
-	echo "\nR2RML_REPO=https://github.com/jaspersnel/r2rml_tests.git" >> .env
-	echo "\nR2RML_REPO_SUBDIR=." >> .env
+	printf "AIRFLOW_VAR_APPEND_CSV=0\n" >> .env
+	printf "\n" >> .env
+	printf "AIRFLOW_UID=50000\n" >> .env
+	printf "AIRFLOW_GID=0\n" >> .env
+	printf "\n" >> .env
+	printf "# R2RML\n" >> .env
+	printf "AIRFLOW_VAR_R2RML_REPO=https://github.com/jaspersnel/r2rml_tests.git\n" >> .env
+	printf "AIRFLOW_VAR_R2RML_REPO_SUBDIR=.\n" >> .env
+	printf "AIRFLOW_VAR_R2RML_DB_URL=postgresql://airflow:airflow@postgres:5432/data\n" >> .env
+	printf "AIRFLOW_VAR_R2RML_RDB_CONNSTR=jdbc:postgresql://localhost:5432/protrait\n" >> .env
+	printf "AIRFLOW_VAR_R2RML_RDB_PASSWORD=passwordhere\n" >> .env
+	printf "AIRFLOW_VAR_R2RML_RDB_USER=postgres\n" >> .env
+	printf "\n" >> .env
+	printf "# Triplestore\n" >> .env
+	printf "AIRFLOW_VAR_SPARQL_ENDPOINT=http://graphdb:7200/repositories/data\n" >> .env
+	printf "\n" >> .env
+	printf "# LibreClinica\n" >> .env
+	printf "AIRFLOW_VAR_LC_ENDPOINT=https://libreclinica.health-ri.nl/lcws_test/ws/\n" >> .env
+	printf "AIRFLOW_VAR_LC_USER=protrait_import\n" >> .env
+	printf "AIRFLOW_VAR_LC_PASSWORD=hashed_password_here" >> .env
 }
+
 if [ "$1" == "sudo" ]
 then
 	echo "Running the process as $1"
 else
 	echo "Running the process as normal user"
 fi
+
 echo "Setting up the environment"
 if [ "$2" == "ubuntu" ]
 then
@@ -62,8 +80,11 @@ then
 else
 	redhat_env_setup
 fi
+
 echo "Downloading the repo and creating the environment file for airflow"
 repo_setup
+
 echo "Creating docker containers"
 docker_setup
+
 echo "Done. Airflow can be accessed on 8080, Graphdb on 7200, term mapper on 5001"
